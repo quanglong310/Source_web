67278f1253524448a3332c2a03ba7223e1c0199e
quanglong310 <quanglong310@gmail.com>
2023-11-21 18:53:05 +0700

    first commit Source_web

---
+++ server/utils/MyConstants.js
@@ -0,0 +1,11 @@
+const MyConstants = {
+    DB_SERVER: 'cluster0.wunxw6q.mongodb.net',
+    DB_USER: 'quanglong310',
+    DB_PASS: '3102003lonGg',
+    DB_DATABASE: 'DOAN_WEB',
+    EMAIL_USER: 'quanglongdateh@gmail.com', // Microsoft mail service
+    EMAIL_PASS: 'anykdrkjyuxbozwi',
+    JWT_SECRET: 'DOAN_WEB',
+    JWT_EXPIRES: '1800000', // in milliseconds
+  };
+  module.exports = MyConstants;
